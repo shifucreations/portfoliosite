eaf779996247428def6288b8ccaa977981127a8e
shifucreations <lisannewille@hotmail.com>
2025-09-06 14:37:46 +0200

    finally working javascript

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const navbarMenu = document.querySelector('.navbar-menu');
 navbarToggle.addEventListener('click', () =>{
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
-})
+});
 
 //accordion
 
@@ -15,8 +15,15 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 accordionItemHeaders.forEach(accordionItemHeader =>{
     accordionItemHeader.addEventListener("click", event =>{
         accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")) {
+          accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        }
+        else {
+          accordionItemBody.style.maxHeight = 0;
+        }
     })
-})
+});
 
 
 
