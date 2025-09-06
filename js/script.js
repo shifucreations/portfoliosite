58acc044ac9eb163fafa767db69bfbe8982c770a
shifucreations <lisannewille@hotmail.com>
2025-09-06 13:55:27 +0200

    accordion

--- js/script.js
+++ js/script.js
@@ -8,7 +8,22 @@ navbarToggle.addEventListener('click', () =>{
     navbarMenu.classList.toggle('active');
 })
 
+//accordion
 
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader =>{
+    accordionItemHeader.addEventListener("click", event =>{
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody= accordionItemHeader.nextElementSibling;
+        if(accordionItemHeader.classList.contains("active")){
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollheight + "px";
+        }
+        else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    })
+})
 
 
 
