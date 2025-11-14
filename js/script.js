8a8083a792af0528221b60b3ca8ffac1a7cff9c0
shifucreations <lisannewille@hotmail.com>
2025-11-14 14:58:14 +0100

    test slider

--- js/script.js
+++ js/script.js
@@ -31,5 +31,10 @@ accordionItemHeaders.forEach(accordionItemHeader =>{
 });
 
 
+//slider image
 
+const container = document.querySelector(".container");
+document.querySelector(".slider").addEventListener("input", (e) => {
+  container.style.setProperty("--position", `${e.target.value}%`);
+});
 
